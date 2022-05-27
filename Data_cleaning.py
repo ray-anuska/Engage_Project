b307c1f867ff968bd8cbfd6e237ab9f9a3e0ef74
ray-anuska <91963539+ray-anuska@users.noreply.github.com>
2022-05-27 17:33:14 +0530

    Comment edits

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -7,13 +7,13 @@ def outliers(df) :
 	for x in df.columns : 
 		missing = df[x].isnull().sum()
 		percent = round(missing / len(df[x]) * 100)
-		if percent > 5 : 
+		if percent > 10 : #if missing values more than 10 percent then drop those columns
 			df = df.drop(columns = x)
 
 	for i in df.columns : 
-		df = df[~df[i].isnull()]
+		df = df[~df[i].isnull()] #dropping rows with empty or missing values
 
-	df = df.reset_index(drop = True)
+	df = df.reset_index(drop = True)#resetting the index after deleting some rows
 	return df
 
 def clean(df) : 
@@ -23,10 +23,11 @@ def clean(df) :
 	n_cols = df.select_dtypes(include = object)
 
 	for column in n_cols.columns : 
-		try : 
-			l = re.findall(r"[^\W\d_]+|\d+", df.loc[0, column])
+		try : #checks if string follows pattern of "number unit" and if it does, removes the unit and makes column numeric
+			#only checks first element of column assuming all will be same
+			l = re.findall(r"[^\W\d_]+|\d+", df.loc[0, column])#separating the letters and digits
 			str = l[len(l) - 1]
-			if str.isdigit() :
+			if str.isdigit() :#if the last element is a digit then it means no unit to remove so onto the next column
 				continue
 			if len(l) == 1 : 
 				continue
@@ -39,13 +40,13 @@ def clean(df) :
 					f = 1
 					break 
 			if f == 0 : 
-				df[column] = [re.sub('[^0-9]+', '', _) for _ in df[column]]
+				df[column] = [re.sub('[^0-9]+', '', _) for _ in df[column]]#keeping only the number and converting column to numeric type
 				df[column] = pd.to_numeric(df[column])
 		except : 
-			df[column] = org_df[column]
+			df[column] = org_df[column]#restor original in case of any error
 
 	for i in df.columns : 
-		df = df[~df[i].isnull()]
+		df = df[~df[i].isnull()]#remove any rows with missing values
 
 	return df
 
